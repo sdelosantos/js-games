4629f3e89423be1f1cb501ff767fd549555b5887
Saddan de los santos <saddam.santos@gmail.com>
2023-11-07 10:27:11 -0400

    refactor: collision detection functions

--- engine/gameObject.js
+++ engine/gameObject.js
@@ -1,11 +1,16 @@
 //@ts-check
 
+import {
+  allocatedSpaceCollisionDetection,
+  boundaryCollisionDetection,
+} from "./engine.utils";
+
 /**
  * @typedef {GameObjectElement} GameObjectElementType
  * @typedef {import('./scene').GameScene} GameSceneType
  * @typedef {{x: number; y: number}} GameObjectPosition
  * @typedef {Array<Array<string | number>>} GameObjectShapeType
- * 
+ *
  * @typedef CollisionType
  * @property {'BOARD_LIMIT' | 'SOLIDIFIED_STUFFED' | 'GAME_OBJECT'} type
  * @property {Array<'LEFT' | 'RIGHT' | 'TOP' | 'BOTTOM'>} side
@@ -14,159 +19,132 @@
  */
 
 export class GameObjectElement {
-    get IS_VISIBLE() {
-        return this.#_visible
+  get IS_VISIBLE() {
+    return this.#_visible;
+  }
+  get DEFAULT_GAME_OBJECT_COLOR() {
+    return "red";
+  }
+
+  /** @type {boolean} */
+  #_visible = true;
+  /** @type {GameObjectShapeType} */
+  #_currentShape = [];
+  /** @type {GameObjectPosition | undefined} */
+  #_currentPosition = undefined;
+  /** @type {GameSceneType | null} */
+  #_sceneInstance = null;
+
+  /** @type {CollisionType | null} */
+  collision = null;
+  /** @type {string | null} */
+  name = null;
+
+  /**
+   * @param {string} name
+   * @param {{position: GameObjectPosition; shape: GameObjectShapeType, isVisible?: boolean }} initGameObject
+   */
+  constructor(name, initGameObject) {
+    this.name = name;
+    this.#_currentShape = [...initGameObject.shape];
+    this.#_currentPosition = { ...initGameObject.position };
+    if (initGameObject.isVisible !== undefined)
+      this.#_visible = initGameObject.isVisible;
+  }
+
+  /**
+   * @param {GameObjectPosition} position
+   * @returns {GameObjectElementType}
+   */
+  setPosition(position) {
+    this.#_currentPosition = position;
+    return this;
+  }
+
+  getPosition() {
+    return this.#_currentPosition;
+  }
+  /**
+   * @param {GameObjectShapeType} shape
+   * @returns {GameObjectElementType}
+   */
+  setShape(shape = []) {
+    this.#_currentShape = [...shape];
+    return this;
+  }
+
+  getCurrentShape() {
+    return this.#_currentShape;
+  }
+  /**
+   * @param {boolean} visible
+   * @returns {GameObjectElement}
+   */
+  setVisibility(visible) {
+    this.#_visible = visible;
+    return this;
+  }
+
+  /**
+   * @returns {GameSceneType | null}
+   */
+  getScene() {
+    return this.#_sceneInstance;
+  }
+
+  /**
+   * @param {GameSceneType} sceneInstance
+   */
+  $injectScene(sceneInstance) {
+    this.#_sceneInstance = sceneInstance;
+  }
+  /**
+   * @returns {Array<GameObjectPosition & {value: string | number}>}
+   */
+  getPoligoneShape() {
+    const newShape = [];
+    const shape = this.#_currentShape;
+
+    shape.forEach((row, yPosition) => {
+      row.forEach((value, xPosition) => {
+        const x = xPosition + (this.#_currentPosition?.x || 0);
+        const y = yPosition + (this.#_currentPosition?.y || 0);
+        newShape.push({ x, y, value });
+      });
+    });
+
+    return newShape;
+  }
+
+  checkCollision({ x, y }, callback) {
+    const scene = this.#_sceneInstance;
+
+    if (scene) {
+      Promise.all([
+        Promise.resolve(
+          boundaryCollisionDetection({
+            poligonObject: this.#_currentShape,
+            poligonPositionX: x,
+            poligonPositionY: y,
+            sceneHeight: scene.boardHeight,
+            sceneWidth: scene.boardWidth,
+          })
+        ),
+        Promise.resolve(
+          allocatedSpaceCollisionDetection({
+            boardArray: scene.getBoardArray(),
+            poligonObject: this.#_currentShape,
+            poligonPositionX: x,
+            poligonPositionY: y,
+          })
+        ),
+      ]).then(([col, col2]) => {
+        const collisiones = [...col, ...col2];
+        collisiones.length > 0 &&
+          this.#_sceneInstance?.emit("onObjectCollision", collisiones);
+        callback(collisiones);
+      });
     }
-    get DEFAULT_GAME_OBJECT_COLOR() { return 'red' }
-
-    /** @type {boolean} */
-    #_visible = true
-    /** @type {GameObjectShapeType} */
-    #_currentShape = [];
-    /** @type {GameObjectPosition | undefined} */
-    #_currentPosition = undefined;
-    /** @type {GameSceneType | null} */
-    #_sceneInstance = null;
-
-    /** @type {CollisionType | null} */
-    collision = null;
-    /** @type {string | null} */
-    name = null;
-
-    /**
-     * @param {string} name 
-     * @param {{position: GameObjectPosition; shape: GameObjectShapeType, isVisible?: boolean }} initGameObject 
-     */
-    constructor(name, initGameObject) {
-        this.name = name;
-        this.#_currentShape = [...initGameObject.shape];
-        this.#_currentPosition = { ...initGameObject.position };
-        if (initGameObject.isVisible !== undefined)
-            this.#_visible = initGameObject.isVisible;
-    }
-
-    /**
-     * @param {GameObjectPosition} position 
-     * @returns {GameObjectElementType}
-     */
-    setPosition(position) {
-        this.#_currentPosition = position;
-        return this;
-    }
-
-    getPosition() {
-        return this.#_currentPosition;
-    }
-    /**
-     * @param {GameObjectShapeType} shape 
-     * @returns {GameObjectElementType}
-     */
-    setShape(shape = []) {
-        this.#_currentShape = [...shape];
-        return this;
-    }
-
-    getCurrentShape() {
-        return this.#_currentShape
-    }
-    /**
-     * @param {boolean} visible 
-     * @returns {GameObjectElement}
-     */
-    setVisibility(visible) {
-        this.#_visible = visible;
-        return this;
-    }
-
-    /**
-     * @returns {GameSceneType | null} 
-     */
-    getScene() {
-        return this.#_sceneInstance;
-    }
-
-    /**
-     * @param {GameSceneType} sceneInstance 
-     */
-    $injectScene(sceneInstance) {
-        this.#_sceneInstance = sceneInstance;
-    }
-    /**
-     * @returns {Array<GameObjectPosition & {value: string | number}>}
-     */
-    getPoligoneShape() {
-        const newShape = [];
-        const shape = this.#_currentShape;
-
-        shape.forEach((row, yPosition) => {
-            row.forEach((value, xPosition) => {
-                const x = xPosition + (this.#_currentPosition?.x || 0);
-                const y = yPosition + (this.#_currentPosition?.y || 0);
-                newShape.push({ x, y, value })
-            })
-        });
-
-        return newShape;
-    }
-
-    checkBoardColision() {
-        const scene = this.#_sceneInstance;
-        const position = this.getPosition();
-        if (scene && position) {
-            const { x: currentX, y: currentY } = position;
-            const characterAreaY = this.#_currentShape.length - 1;
-            const characterAreaX = this.#_currentShape[0].length - 1;
-
-            const adjacentX = currentX + characterAreaX, adjacentY = currentY + characterAreaY;
-            const RightBoardLimit = scene.boardWidth - 1;
-            const BottomBoardLimit = scene.boardHeight - 1;
-
-            let sideCollision = {
-                LEFT: currentX <= 0,
-                RIGHT: adjacentX >= RightBoardLimit,
-                TOP: currentY <= 0,
-                BOTTOM: adjacentY >= BottomBoardLimit,
-            }
-            if (sideCollision.LEFT || sideCollision.RIGHT || sideCollision.BOTTOM || sideCollision.TOP) {
-                const allCollisions = Object.keys(sideCollision).filter(ky => sideCollision[ky]);
-                this.collision = {
-                    gameObject: null,
-                    type: 'BOARD_LIMIT',
-                    coordinates: [{ x: adjacentX, y: adjacentY }],
-                    // @ts-ignore
-                    side: allCollisions
-                }
-            } else {
-                const board = this.#_sceneInstance?.getBoardArray();
-                const yPosition = adjacentY + 1;
-                // @ts-ignore
-                const xPosition = this.#_currentShape.slice(-1)[0].reduce((pre, xValue, x) => {
-                    // @ts-ignore
-                    if (xValue) pre.push(x + currentX)
-                    return pre;
-                }, []);
-
-                const isOnCollision = board && board[yPosition].some((xRows, x) => {
-                    return xRows && xPosition.includes(x);
-                });
-
-                if (isOnCollision) {
-                    this.collision = {
-                        gameObject: null,
-                        type: 'SOLIDIFIED_STUFFED',
-                        coordinates: [{ x: adjacentX, y: adjacentY }],
-                        // @ts-ignore
-                        side: ['BOTTOM']
-                    }
-                } else {
-                    this.collision = null;
-                }
-            }
-            if (this.collision) {
-                this.#_sceneInstance?.emit('onObjectCollision', this.collision)
-            }
-
-        }
-        return this;
-    }
+    return this;
+  }
 }
--- engine/scene.js
+++ engine/scene.js
@@ -11,245 +11,246 @@
  * @property {boolean?} disableRunner - Disable the screen update loop
  */
 class SceneEventHandle {
-    /** @type {{[k in EventType]: Array<(data:any)=>void>}} */
-    #_eventCallbackPool;
-    constructor() {
-        // @ts-ignore
-        this.#_eventCallbackPool = {};
+  /** @type {{[k in EventType]: Array<(data:any)=>void>}} */
+  #_eventCallbackPool;
+  constructor() {
+    // @ts-ignore
+    this.#_eventCallbackPool = {};
+  }
+  /**
+   * Add event listener
+   * @param {EventType} event
+   * @param {(data:any)=>void} callback
+   */
+  on(event, callback) {
+    if (!this.#_eventCallbackPool[event]) {
+      this.#_eventCallbackPool[event] = [];
     }
-    /**
-     * Add event listener
-     * @param {EventType} event 
-     * @param {(data:any)=>void} callback 
-     */
-    on(event, callback) {
-        if (!this.#_eventCallbackPool[event]) {
-            this.#_eventCallbackPool[event] = [];
-        }
-        this.#_eventCallbackPool[event].push(callback);
-    }
+    this.#_eventCallbackPool[event].push(callback);
+  }
 
-    /**
-     * Dispatch a custome events
-    * @param {EventType} event 
-    * @param {any} data 
-    */
-    emit(event, data) {
-        const listeners = this.#_eventCallbackPool[event];
-        if (listeners) {
-            listeners.forEach((callback) => {
-                callback(data);
-            })
-        }
+  /**
+   * Dispatch a custome events
+   * @param {EventType} event
+   * @param {any} data
+   */
+  emit(event, data) {
+    const listeners = this.#_eventCallbackPool[event];
+    if (listeners) {
+      listeners.forEach((callback) => {
+        callback(data);
+      });
     }
+  }
 }
 export class GameScene extends SceneEventHandle {
-    get DEFAULT_BOARD_COLOR() { return '#ffef45'; }
-    get DEFAULT_SCENE_BG() { return '#000' }
+  get DEFAULT_BOARD_COLOR() {
+    return "#ffef45";
+  }
+  get DEFAULT_SCENE_BG() {
+    return "#000";
+  }
 
-    /** @type {{[k:string]: GameObjectType}} */
-    #gameObjectPool;
-    /** @type {number} */
-    #blockSize;
-    /** @type {GameSceneOptions} */
-    options;
-    /** @type {Array<Array<number | string>>} */
-    boardArray;
-    /** @type {number} */
-    boardWidth;
-    /** @type {number} */
-    boardHeight;
-    /** @type {number} */
-    canvasWidth;
-    /** @type {number} */
-    canvasHeight;
-    /** @type {CanvasRenderingContext2D | null}} */
-    canvasContext = null;
+  /** @type {{[k:string]: GameObjectType}} */
+  #gameObjectPool;
+  /** @type {number} */
+  #blockSize;
+  /** @type {GameSceneOptions} */
+  options;
+  /** @type {Array<Array<number | string>>} */
+  boardArray;
+  /** @type {number} */
+  boardWidth;
+  /** @type {number} */
+  boardHeight;
+  /** @type {number} */
+  canvasWidth;
+  /** @type {number} */
+  canvasHeight;
+  /** @type {CanvasRenderingContext2D | null}} */
+  canvasContext = null;
 
-    /**
-     * @param {GameSceneOptions} options
-     */
-    constructor(options) {
-        super();
-        this.options = options;
-        this.boardArray = [];
-        this.#gameObjectPool = {};
-        this.#blockSize = 20;
+  /**
+   * @param {GameSceneOptions} options
+   */
+  constructor(options) {
+    super();
+    this.options = options;
+    this.boardArray = [];
+    this.#gameObjectPool = {};
+    this.#blockSize = 20;
 
-        this.setBoardSize(options.width, options.height);
+    this.setBoardSize(options.width, options.height);
+  }
+
+  /**
+   * @param {number} blockSize
+   * @returns {GameScene}
+   */
+  setBoardBlockSize(blockSize) {
+    this.#blockSize = blockSize;
+    return this;
+  }
+
+  /**  @returns {Array<Array<number | string>>} */
+  getBoardArray() {
+    return this.boardArray;
+  }
+
+  /**
+   * Define canvas element size and board array size
+   * @param {number} width - set width size in block units
+   * @param {number} height - set height size in block units
+   * @returns {GameScene}
+   */
+  setBoardSize(width, height) {
+    this.boardWidth = width;
+    this.boardHeight = height;
+    this.canvasWidth = width * this.#blockSize;
+    this.canvasHeight = height * this.#blockSize;
+
+    this.boardArray = new Array(height).fill(new Array(width));
+    return this;
+  }
+
+  /**
+   * Register a Game Object inside the scene
+   * @param {GameObjectType} gameObject
+   * @returns {GameScene}
+   */
+  addGameObject(gameObject) {
+    gameObject.$injectScene(this);
+    if (gameObject.name) this.#gameObjectPool[gameObject.name] = gameObject;
+    else throw "much be set a name to the game object";
+    return this;
+  }
+
+  /**
+   * Remove a Game object of the scene
+   * @param {string} name - game object key name
+   * @returns {GameScene} - game object instance
+   */
+  removeGameObject(name) {
+    delete this.#gameObjectPool[name];
+    return this;
+  }
+
+  /**
+   * Get a game object by name
+   * @param {string} name
+   * @returns {GameObjectType}
+   */
+  getGameObject(name) {
+    return this.#gameObjectPool[name];
+  }
+
+  /**
+   * Get all registered game objects
+   * @returns {GameObjectType[]}
+   */
+  getAllGameObject() {
+    return Object.entries(this.#gameObjectPool).map(
+      ([, gameObject]) => gameObject
+    );
+  }
+  /**
+   * Create the canvas context based on the setting provided
+   * @param {HTMLCanvasElement} canvas
+   * @returns {GameScene}
+   */
+  build(canvas) {
+    this.canvasContext = this.#getContent(canvas);
+    return this;
+  }
+
+  /**
+   * Draw on  the canvas the scene with all game objects registered
+   */
+  render() {
+    this.#cleanCanvas().#drawSceneBoard().#drawGameObjects();
+  }
+
+  /**
+   * Build and return the scene's canvas context
+   * @param {HTMLCanvasElement} canvas - canvas elements to get the context
+   * @returns {CanvasRenderingContext2D | null}
+   */
+  #getContent(canvas) {
+    const context = canvas.getContext("2d");
+    canvas.width = this.canvasWidth;
+    canvas.height = this.canvasHeight;
+
+    if (context) {
+      context.fillStyle = this.options.bgColor || this.DEFAULT_SCENE_BG;
+      context?.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+      context.scale(this.#blockSize, this.#blockSize);
     }
 
-    /**
-     * @param {number} blockSize 
-     * @returns {GameScene}
-     */
-    setBoardBlockSize(blockSize) {
-        this.#blockSize = blockSize;
-        return this;
+    return context;
+  }
+
+  /**
+   * Clean all rendered elements on the canvas
+   * @returns {GameScene}
+   */
+  #cleanCanvas() {
+    const context = this.canvasContext;
+    if (context) {
+      context.fillStyle = this.options.bgColor || this.DEFAULT_SCENE_BG;
+      context?.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
     }
 
-    /**  @returns {Array<Array<number | string>>} */
-    getBoardArray() {
-        return this.boardArray;
+    return this;
+  }
+
+  /**
+   * Draw the registered game object on the canvas
+   * @returns {GameScene}
+   */
+  #drawGameObjects() {
+    if (this.canvasContext) {
+      const context = this.canvasContext;
+      Object.entries(this.#gameObjectPool).forEach(([, gameObject]) => {
+        const shape = gameObject.getPoligoneShape();
+
+        shape.forEach(({ x, y, value }) => {
+          const blockColor =
+            typeof value === "string"
+              ? value
+              : gameObject.DEFAULT_GAME_OBJECT_COLOR;
+          if (gameObject.IS_VISIBLE) {
+            if (value) {
+              if (context) {
+                context.fillStyle = blockColor;
+                context.fillRect(x, y, 1, 1);
+              }
+            }
+          }
+        });
+      });
     }
+    return this;
+  }
 
-    /**
-     * Define canvas element size and board array size
-     * @param {number} width - set width size in block units
-     * @param {number} height - set height size in block units
-     * @returns {GameScene}
-     */
-    setBoardSize(width, height) {
-        this.boardWidth = width;
-        this.boardHeight = height;
-        this.canvasWidth = width * this.#blockSize;
-        this.canvasHeight = height * this.#blockSize;
+  /**
+   * Draw the board on the canvas
+   * @returns {GameScene}
+   */
+  #drawSceneBoard() {
+    const context = this.canvasContext;
+    this.boardArray.forEach((row, y) => {
+      row.forEach((valueRgb, x) => {
+        if (valueRgb) {
+          const blockColor =
+            typeof valueRgb === "string" ? valueRgb : this.DEFAULT_BOARD_COLOR;
 
-        this.boardArray = new Array(height).fill(new Array(width));
-        return this;
-    }
-
-    /**
-     * Register a Game Object inside the scene
-     * @param {GameObjectType} gameObject 
-     * @returns {GameScene}
-     */
-    addGameObject(gameObject) {
-        gameObject.$injectScene(this);
-        if (gameObject.name)
-            this.#gameObjectPool[gameObject.name] = gameObject;
-        else
-            throw 'much be set a name to the game object'
-        return this;
-    }
-
-    /**
-     * Remove a Game object of the scene
-     * @param {string} name - game object key name
-     * @returns {GameScene} - game object instance
-     */
-    removeGameObject(name) {
-        delete this.#gameObjectPool[name];
-        return this;
-    }
-
-    /**
-     * Get a game object by name
-     * @param {string} name 
-     * @returns {GameObjectType}
-     */
-    getGameObject(name) {
-        return this.#gameObjectPool[name];
-    }
-
-    /**
-     * Get all registered game objects
-     * @returns {GameObjectType[]}
-     */
-    getAllGameObject() {
-        return Object.entries(this.#gameObjectPool).map(([, gameObject]) => gameObject);
-    }
-    /**
-     * Create the canvas context based on the setting provided
-     * @param {HTMLCanvasElement} canvas 
-     * @returns {GameScene}
-     */
-    build(canvas) {
-        this.canvasContext = this.#getContent(canvas);
-        return this;
-    }
-
-    /**
-     * Draw on  the canvas the scene with all game objects registered
-     */
-    render() {
-        this.#cleanCanvas()
-            .#drawSceneBoard()
-            .#drawGameObjects();
-    }
-
-    /**
-     * Build and return the scene's canvas context
-     * @param {HTMLCanvasElement} canvas - canvas elements to get the context
-     * @returns {CanvasRenderingContext2D | null}
-     */
-    #getContent(canvas) {
-        const context = canvas.getContext('2d');
-        canvas.width = this.canvasWidth;
-        canvas.height = this.canvasHeight;
-
-        if (context) {
-            context.fillStyle = this.options.bgColor || this.DEFAULT_SCENE_BG;
-            context?.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
-            context.scale(this.#blockSize, this.#blockSize);
+          if (context) {
+            context.fillStyle = blockColor;
+            context.fillRect(x, y, 1, 1);
+          }
         }
-
-        return context;
-    }
-
-    /**
-     * Clean all rendered elements on the canvas
-     * @returns {GameScene}
-     */
-    #cleanCanvas() {
-        const context = this.canvasContext;
-        if (context) {
-            context.fillStyle = this.options.bgColor || this.DEFAULT_SCENE_BG;
-            context?.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
-        }
-
-        return this;
-    }
-
-
-    /**
-     * Draw the registered game object on the canvas
-     * @returns {GameScene}
-     */
-    #drawGameObjects() {
-        if (this.canvasContext) {
-            const context = this.canvasContext;
-            Object.entries(this.#gameObjectPool).forEach(([, gameObject]) => {
-                const shape = gameObject.checkBoardColision().getPoligoneShape();
-
-                shape.forEach(({ x, y, value }) => {
-                    const blockColor = typeof value === 'string'
-                        ? value
-                        : gameObject.DEFAULT_GAME_OBJECT_COLOR;
-                    if (gameObject.IS_VISIBLE) {
-                        if (value) {
-                            if (context) {
-                                context.fillStyle = blockColor;
-                                context.fillRect(x, y, 1, 1);
-                            }
-                        }
-                    }
-                })
-            })
-        }
-        return this;
-    }
-
-    /**
-     * Draw the board on the canvas
-     * @returns {GameScene}
-     */
-    #drawSceneBoard() {
-        const context = this.canvasContext;
-        this.boardArray.forEach((row, y) => {
-            row.forEach((valueRgb, x) => {
-                if (valueRgb) {
-                    const blockColor = typeof valueRgb === 'string'
-                        ? valueRgb
-                        : this.DEFAULT_BOARD_COLOR;
-
-                    if (context) {
-                        context.fillStyle = blockColor;
-                        context.fillRect(x, y, 1, 1);
-                    }
-                }
-            })
-        })
-        return this;
-    }
+      });
+    });
+    return this;
+  }
 }
